b599b046e8a6a0bcbce7dd70d81ff91e71ae3dc4
Stefan Vlajkovic <vlajkovicstefan01@gmail.com>
2022-05-16 19:39:53 +0200

    Added cross origin

--- src/main/java/com/example/RedditClone/Controller/PostController.java
+++ src/main/java/com/example/RedditClone/Controller/PostController.java
@@ -6,6 +6,7 @@ import com.example.RedditClone.Service.PostService;
 import com.example.RedditClone.Util.ExtendedModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,6 +27,7 @@ public class PostController {
     }
 
     @GetMapping
+    @CrossOrigin
     public ResponseEntity<List<PostGetAllResponseDTO>> getPosts() {
 
         List<Post> posts = postService.findAll();
